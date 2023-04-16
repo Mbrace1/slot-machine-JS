803e2d29571abd197656d6d6619a5dcfdba3e5c7
Michael Bracey <michaelbracey24@gmail.com>
2023-04-16 14:40:27 +0100

    starting code for spinning reels

--- index.js
+++ index.js
@@ -16,18 +16,68 @@ class slotMachine {
         this.symbols = symbols;
         this.spinDuration = spinDuration * 1000; // convert to secs
         this.slotResult = [];
+        this.tl = gsap.timeline();
+        this.slotWidth;
+        this.slotHeight;
+        this.symbolWidth;
+        this.symbolHeight;
+        this.allSlots = document.querySelectorAll('.slot-reel');
+
+        this.setup();
+        debugger
+    }
+
+    setup() {
+        this.tl.pause();
+        this.getSlotMachineProperties();
+        this.setUpSpinAnimation();
+    }
+
+    getSlotMachineProperties() {
+        const SlotMachineProperties = document.getElementsByClassName("slot-reel")[0].getBoundingClientRect();
+        this.slotWidth = SlotMachineProperties.width;
+        this.slotHeight = SlotMachineProperties.height;
+        const SymbolProperties = document.getElementsByClassName("slot-symbol")[0].getBoundingClientRect();
+        this.symbolWidth = SymbolProperties.width;
+        this.symbolHeight = SymbolProperties.height;
+    }
+    //TODO resize function for slotmachine on diff devices
+    //TODO create function to build up html
+
+    setUpSpinAnimation() {
+        //TODO need to figure out wrapping
+        //TODO land on whole symbol
+        // how does wrap work
+        const totalHeight= 5*this.symbolHeight;
+        const wrapOffsetTop = this.symbolHeight/-2;
+        const wrapOffsetBottom = totalHeight+wrapOffsetTop;
+        var wrap = gsap.utils.wrap(wrapOffsetTop,wrapOffsetBottom);
+        const yheight = "-=" + totalHeight;
+            debugger
+            this.tl.to(".slot-symbol", {
+                duration: 2,
+                ease: "none",
+                y: yheight,
+                x: "0",
+                modifiers: {
+                  y: gsap.utils.unitize(wrap),
+                },
+                repeat: -1
+            });
     }
 
     startSpin() {
         this.emptyPrevSlotResult();
         // TODO figure out how to tween sprites to move and blur
-        const waitForDuration = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve("");
-            }, this.spinDuration); 
-        }).then(() => {
+        // this.allSlots.forEach((slot) => {
+        //   slot.style.transition = 'none';
+        //   slot.style.transform = 'translateY(0)';
+        // });
+        this.tl.play();
+        setTimeout(() => {
             this.generateResult();
-        })
+            this.tl.pause();
+        }, this.spinDuration);
     }
 
     stopSpin() {
@@ -55,14 +105,6 @@ class slotMachine {
         // TODO here we check for any wins by matching this.slotResult with 3 in a row
         // then check value of win and parse this data
 
-        // ways to win:
-        // xxx
-        //x
-        // x
-        //  x
-        //x
-        // xx
-
         this.stopSpin();
     }
 
@@ -75,7 +117,7 @@ class slotMachine {
                 //TODO will change this too sprite later rather than number
                 let slotSymbolToChange = slotReel.getElementsByClassName(`slot-position-${j.toString()}`);
                 slotSymbolToChange[0].innerHTML = this.slotResult[i][j].toString();
-                debugger
+                slotSymbolToChange[0].style.backgroundColor = listOfSymbols[this.slotResult[i][j]];
             }
         }
         console.log("display result")
@@ -97,7 +139,7 @@ class slotMachine {
     }
 }
 
-const mySlotMachine = new slotMachine(3, 5, listOfSymbols, 1);
+const mySlotMachine = new slotMachine(3, 5, listOfSymbols, 6);
 // let number = 1;
 // const test = document.getElementById(`slot-reel-${number.toString()}`)
 // debugger
